339a9a07e84e3f250b18c990d7747ce801f18560
Fariko Kas <kasvzelst@gmail.com>
2020-03-08 13:51:37 +0100

    Added some notes

--- Assets/Scripts/kascodingNL/Tests/TestDetection.cs
+++ Assets/Scripts/kascodingNL/Tests/TestDetection.cs
@@ -3,9 +3,15 @@ using UnityEngine;
 
 class TestDetection : Checks
 {
+    /*
+     * Does your class derive from MonoBehaviour? No problemo! The Checks.cs class also derives from MonoBehaviour, so you can just use the functions like normal.
+     * Do you use a custom MonoBehaviour adoption? Then you will need to implement this in a other class that doesn't have to 
+     * derive from that specific class!
+     */
 
     private void Awake()
     {
+        Debug.Log("See? You can just use all things!");
     }
 
     public override void ClockDesync(int TimeDiff)
