b334ccb8feeeacc50b25c81e9b16600217c34bdf
kasvzelst@gmail.com <kasvzelst@gmail.com>
2020-03-30 15:33:53 +0200

    Changed the socket! NOT TESTED, MIGHT BREAK

--- Socket/index.js
+++ Socket/index.js
@@ -4,12 +4,17 @@ const ws = new WebSocket.Server({ port: 8180 });
 
 ws.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
-  	//var filtered = message.toString().replace(/([^a-z0-9]+)/gi, '');
   	var filtered = message.toString().replace(/[^\x20-\x7E]/g, '');
     console.log('received: %s', filtered);
+	
+	if(filtered == "debuggertrue")
+	{
+		ws.send("debugquit");
+	}
+	else
+	{
+		ws.send("continue");
   });
-
-  ws.send(126124);
 });
 
 ws.on('close', function disconnect(reasonCode, description)
