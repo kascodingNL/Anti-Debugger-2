705d0319b729bbccc4ee8680209ccc6b72101f19
kasvzelst@gmail.com <kasvzelst@gmail.com>
2020-03-31 14:57:43 +0200

    V1.0 Changes: Changed push .bat

--- gitpush.bat
+++ gitpush.bat
@@ -1,4 +1,7 @@
 git add .
 set /p message=Enter a message: 
-git commit -m "%message%"
+set /p version=What is the version? 
+git commit -m "V%version% Changes: %message%"
 git push origin master
+
+echo "Pushed %version% with Message: %message%"
