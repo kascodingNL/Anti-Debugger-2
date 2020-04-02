012e06ba5b9b62f923ac20a24d59c67b87fd2e73
kasvzelst@gmail.com <kasvzelst@gmail.com>
2020-04-02 22:09:05 +0200

    V1.2 Changes: Added License warning

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ Create an issue with the prefix [REQUEST] YourAwesomeFeature!
 Upcoming features:
 
 - WebSocket server so the server can exchange encryption keys(for secure data storing), and for memory integrity checks!
+
+
+License:
+If I discover that you use this project, without STRICTLY following the MIT license, I WILL take it down!
--- Socket/index.js
+++ Socket/index.js
@@ -1,11 +1,29 @@
 const WebSocket = require('ws');
 
 const ws = new WebSocket.Server({ port: 8180 });
+var connectedArray = {};
+
+console.log('Started on port 8180!');
 
 ws.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
-  	var filtered = message.toString().replace(/[^\x20-\x7E]/g, '');
-    console.log('received: %s', filtered);
+	  const filtered = message.toString().replace(/[^\x20-\x7E]/g, '');
+	  console.log('received: %s', filtered);
+	var splitted = filtered.split(' ');
+
+	if(splitted[1] == "handshake" && splitted[0] == "sending")
+	{
+		connectedArray.push(filtered);
+	}
+
+	if(splitted[1] == "handshake" && splitted[0] == "disconnect")
+	{
+		const index = connectedArray.indexOf(filtered);
+		if(index > -1)
+		{
+			connectedArray.splice(index, 1);
+		}
+	}
 	
 	if(filtered == "debuggertrue")
 	{
